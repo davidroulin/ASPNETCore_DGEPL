0ac6f56393cf5ebb4f74280a29f20b8916de4365
David Roulin <freelance@davidroulin.ch>
2024-01-24 15:50:00 +0100

    on commence a boucler cote serveur

--- FilmDBWebApp/Pages/Index.cshtml.cs
+++ FilmDBWebApp/Pages/Index.cshtml.cs
@@ -3,14 +3,31 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FilmDBWebApp.Pages
 {
-	public class IndexModel: PageModel
+	public class IndexModel : PageModel
 	{
-		private readonly ILogger<IndexModel> _logger;
+
+		public string TitreTest { get; set; }
+
+        public List<string> tousLesFilms_CoteServeur;
+
+        private readonly ILogger<IndexModel> _logger;
 
 		public IndexModel(ILogger<IndexModel> logger)
 		{
 			_logger = logger;
-		}
+
+			////  TODO en dur 4 films pour tester
+			tousLesFilms_CoteServeur = new List<string>()
+			{
+				"Rocky 14",
+				"Rambo 12",
+				"La petite maison dans la prairie",
+				"Shakespeare in love"
+			};
+
+			TitreTest = "Rocky 12";
+
+        }
 
 		public void OnGet()
 		{
