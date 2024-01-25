83ae97c4d9d9a3b3e74e08a9e29347fe962de79d
David Roulin <freelance@davidroulin.ch>
2024-01-25 11:25:01 +0100

    ca marche ! avec Json deserialize

--- FilmDBWebApp/Film.cs
+++ FilmDBWebApp/Film.cs
@@ -1,13 +1,46 @@
 ﻿using System;
+using System.Text.Json.Serialization;
+
+/// <summary>
+/// 
+/// Exemple
+/// 
+// {
+//	  "title": "102 Dalmatians",
+//    "year": 2000,
+//    "cast": [
+//      "Glenn Close",
+//      "Gérard Depardieu",
+//      "Alice Evans"
+//    ],
+//    "genres": [
+//      "Comedy",
+//      "Family",
+//      "Crime"
+//    ],
+//    "href": "102_Dalmatians",
+//    "extract": "102 Dalmatians is a 2000 American crime comedy film directed by Kevin Lima and produced by Edward S. Feldman and Walt Disney Pictures. The sequel to the 1996 film 101 Dalmatians, a live-action remake of the 1961 Disney animated film of the same name, it stars Glenn Close reprising her role as Cruella de Vil as she attempts to steal puppies for her \"grandest\" fur coat yet. Glenn Close and Tim McInnerny were the only two actors from the 1996 film to return for the sequel. The film received generally negative reviews from critics and was unable to achieve the box office success of its predecessor, although the film was nominated for the Academy Award for Best Costume Design.",
+//    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/f/fe/102_dalmatians_poster.jpg",
+//    "thumbnail_width": 220,
+//    "thumbnail_height": 327
+//  }
+/// 
+/// 
+/// </summary>
 
 public class Film
 {
 
 	public string Title { get; set; }
 
-	public string Summary { get; set; }
+	[JsonPropertyName("extract")]
+	public string Summary { get; set; } // "extract"
 
-	public string Year { get; set; }
+	public int Year { get; set; }
 
-	
+	[JsonPropertyName("thumbnail")]
+	public string Poster{ get; set; } // "thumbnail"
+
+
+
 }
--- FilmDBWebApp/Pages/Index.cshtml.cs
+++ FilmDBWebApp/Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+using System.Text.Json;
+using Microsoft.AspNetCore.Hosting;
 
 namespace FilmDBWebApp.Pages
 {
@@ -13,9 +16,13 @@ namespace FilmDBWebApp.Pages
 
 		private readonly ILogger<IndexModel> _logger;
 
-		public IndexModel(ILogger<IndexModel> logger)
+		private readonly IWebHostEnvironment _hostingInfo;
+
+		public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment hostingInfo)
 		{
 			_logger = logger;
+
+			_hostingInfo = hostingInfo;
 
 			////  TODO en dur 4 films pour tester
 			//tousLesFilms_CoteServeur = new List<string>()
@@ -26,16 +33,38 @@ namespace FilmDBWebApp.Pages
 			//	"Shakespeare in love"
 			//};
 
-			tousLesFilms_CoteServeur = new List<Film>()
-			{
-				new Film{ Summary = "Super film", Title = "Alien 17", Year = "2021" },
-				new Film{ Summary = "Film moyen", Title = "La petite sirene, la revanche", Year = "1999" }
-			};
+			//tousLesFilms_CoteServeur = new List<Film>()
+			//{
+			//	new Film{ Summary = "Super film", Title = "Alien 17", Year = "2021" },
+			//	new Film{ Summary = "Film moyen", Title = "La petite sirene, la revanche", Year = "1999" }
+			//};
+
 
 
 			//TitreTest = "Rocky 12";
 
-        }
+			string cheminFichierData = Path.Combine(
+				_hostingInfo.WebRootPath,
+				"data",
+				"movies.json"
+			);
+			// TODO tester ~
+			// TODO WebHostEnvironment
+
+			var pointeurFichierData = System.IO.File.OpenText(cheminFichierData);
+
+			string contenuFichierData = pointeurFichierData.ReadToEnd();
+
+			Film[]? films = JsonSerializer.Deserialize<Film[]>(
+				contenuFichierData,
+				new JsonSerializerOptions {
+					PropertyNameCaseInsensitive = true
+				}
+			);
+
+			tousLesFilms_CoteServeur = films.ToList();
+
+		}
 
 		public void OnGet()
 		{
