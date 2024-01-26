156e0e0b38b370b002c693a936d1806f9f9f32c2
David Roulin <freelance@davidroulin.ch>
2024-01-26 14:49:01 +0100

    nettoyage de toute la partie Json (pour faire de la BDD)

--- FilmDBWebApp/Models/Film.cs
+++ FilmDBWebApp/Models/Film.cs
@@ -1,33 +1,4 @@
-﻿using FilmDBWebApp.Models.Utils;
-using System;
-using System.Text.Json.Serialization;
-
-/// <summary>
-/// 
-/// Exemple
-/// 
-// {
-//	  "title": "102 Dalmatians",
-//    "year": 2000,
-//    "cast": [
-//      "Glenn Close",
-//      "Gérard Depardieu",
-//      "Alice Evans"
-//    ],
-//    "genres": [
-//      "Comedy",
-//      "Family",
-//      "Crime"
-//    ],
-//    "href": "102_Dalmatians",
-//    "extract": "102 Dalmatians is a 2000 American crime comedy film directed by Kevin Lima and produced by Edward S. Feldman and Walt Disney Pictures. The sequel to the 1996 film 101 Dalmatians, a live-action remake of the 1961 Disney animated film of the same name, it stars Glenn Close reprising her role as Cruella de Vil as she attempts to steal puppies for her \"grandest\" fur coat yet. Glenn Close and Tim McInnerny were the only two actors from the 1996 film to return for the sequel. The film received generally negative reviews from critics and was unable to achieve the box office success of its predecessor, although the film was nominated for the Academy Award for Best Costume Design.",
-//    "thumbnail": "https://upload.wikimedia.org/wikipedia/en/f/fe/102_dalmatians_poster.jpg",
-//    "thumbnail_width": 220,
-//    "thumbnail_height": 327
-//  }
-/// 
-/// 
-/// </summary>
+﻿using System;
 
 namespace FilmDBWebApp.Models
 {
@@ -41,22 +12,10 @@ namespace FilmDBWebApp.Models
 
 		public string Title { get; set; }
 
-		[JsonPropertyName("extract")]
 		public string Summary { get; set; } // "extract"
 
-		public string TruncatedSummary
-		{
-			get
-			{
-
-				//if (Summary == null)
-				//{
-				//	return "(no resume)";
-				//} else
-				//{
-				//	return Summary.Substring(0, 150);
-				//}
-
+		public string TruncatedSummary {
+			get {
 				return (Summary?.Substring(0, Math.Min(max_length_summary, Summary.Length)) ?? "(no resume)")
 					+ (Summary?.Length > max_length_summary ? "..." : "" ?? "");
 			}
@@ -64,14 +23,11 @@ namespace FilmDBWebApp.Models
 
 		public int Year { get; set; }
 
-		[JsonPropertyName("thumbnail")]
 		public string Poster { get; set; } // "thumbnail"
 
-		[JsonConverter(typeof(JsonToActorsConverter))]
-		[JsonPropertyName("cast")]
-		public List<Actor> Actors { get; set; }
+		//public List<Actor> Actors { get; set; }
 
-		public List<string> Genres { get; set; }
+		//public List<string> Genres { get; set; }
 
 	}
 
--- FilmDBWebApp/Pages/Index.cshtml.cs
+++ FilmDBWebApp/Pages/Index.cshtml.cs
@@ -10,60 +10,13 @@ namespace FilmDBWebApp.Pages
 	public class IndexModel : PageModel
 	{
 
-		//public string TitreTest { get; set; }
-
-        //public List<string> tousLesFilms_CoteServeur;
-        public List<Film> tousLesFilms_CoteServeur;
-
 		private readonly ILogger<IndexModel> _logger;
 
-		private readonly IWebHostEnvironment _hostingInfo;
+		public List<Film> tousLesFilms_CoteServeur = new List<Film>();
 
-		public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment hostingInfo)
+		public IndexModel(ILogger<IndexModel> logger)
 		{
 			_logger = logger;
-
-			_hostingInfo = hostingInfo;
-
-			////  TODO en dur 4 films pour tester
-			//tousLesFilms_CoteServeur = new List<string>()
-			//{
-			//	"Rocky 14",
-			//	"Rambo 12",
-			//	"La petite maison dans la prairie",
-			//	"Shakespeare in love"
-			//};
-
-			//tousLesFilms_CoteServeur = new List<Film>()
-			//{
-			//	new Film{ Summary = "Super film", Title = "Alien 17", Year = "2021" },
-			//	new Film{ Summary = "Film moyen", Title = "La petite sirene, la revanche", Year = "1999" }
-			//};
-
-
-
-			//TitreTest = "Rocky 12";
-
-			string cheminFichierData = Path.Combine(
-				_hostingInfo.WebRootPath,
-				"data",
-				"movies.json"
-			);
-			// TODO tester ~
-			// TODO WebHostEnvironment
-
-			var pointeurFichierData = System.IO.File.OpenText(cheminFichierData);
-
-			string contenuFichierData = pointeurFichierData.ReadToEnd();
-
-			Film[]? films = JsonSerializer.Deserialize<Film[]>(
-				contenuFichierData,
-				new JsonSerializerOptions {
-					PropertyNameCaseInsensitive = true
-				}
-			);
-
-			tousLesFilms_CoteServeur = films.ToList();
 
 		}
 
